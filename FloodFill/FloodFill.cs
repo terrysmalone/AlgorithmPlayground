c252cd3b6d7d805f3c37fdd47a2464a9d7f2652e
Terry Malone <terrysmalone@hotmail.com>
2026-03-26 13:30:24 +0000

    Add flood fill

---
+++ FloodFill/FloodFill.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+
+namespace FloodFill;
+
+internal class FloodFill
+{
+    private readonly bool[,] _grid;
+
+    internal FloodFill(bool[,] grid)
+    {
+        _grid = grid;
+    }
+
+    internal int GetAvailableSpace(Point point)
+    {
+        // Bounds check
+        if (point.X < 0 || point.X >= _grid.GetLength(0) || point.Y < 0 || point.Y >= _grid.GetLength(1))
+        {
+            return 0;
+        }
+
+        var visited = new HashSet<Point>();
+        var toCheck = new Queue<Point>();
+
+        toCheck.Enqueue(point);
+
+        while (toCheck.Count > 0)
+        {
+            Point checkPoint = toCheck.Dequeue();
+
+            foreach (Point adjacentPoint in GetAdjacentPoints(checkPoint))
+            {
+                if (!visited.Contains(adjacentPoint)
+                    && !_grid[adjacentPoint.X, adjacentPoint.Y])
+                {
+                    toCheck.Enqueue(adjacentPoint);
+                    visited.Add(adjacentPoint);
+                }
+            }
+        }
+
+        return visited.Count;
+    }
+
+    private IEnumerable<Point> GetAdjacentPoints(Point checkPoint)
+    {
+        var adjacentPoints = new List<Point>();
+
+        // Up
+        if (checkPoint.Y > 0)
+        {
+            adjacentPoints.Add(new Point(checkPoint.X, checkPoint.Y - 1));
+        }
+
+        // Down
+        if (checkPoint.Y < _grid.GetLength(1) - 1)
+        {
+            adjacentPoints.Add(new Point(checkPoint.X, checkPoint.Y + 1));
+        }
+
+        // Left
+        if (checkPoint.X > 0)
+        {
+            adjacentPoints.Add(new Point(checkPoint.X - 1, checkPoint.Y));
+        }
+
+        // Right
+        if (checkPoint.X < _grid.GetLength(0) - 1)
+        {
+            adjacentPoints.Add(new Point(checkPoint.X + 1, checkPoint.Y));
+        }
+
+        return adjacentPoints;
+    }
+}
