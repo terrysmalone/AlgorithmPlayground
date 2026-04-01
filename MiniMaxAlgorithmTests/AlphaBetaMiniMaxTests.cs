55bdbf496d5f237d14f0a985ae9763536a36a965
Terry Malone <terrysmalone@hotmail.com>
2026-04-01 12:14:28 +0100

    Update AlphaBetaMiniMaxTests.cs

--- MiniMaxAlgorithmTests/AlphaBetaMiniMaxTests.cs
+++ MiniMaxAlgorithmTests/AlphaBetaMiniMaxTests.cs
@@ -5,17 +5,27 @@ using NUnit.Framework;
 
 namespace MiniMaxAlgorithmTests;
 
-[TestFixture(false, TestName = "AlphaBeta_WithoutMoveOrdering")]
-[TestFixture(true, TestName = "AlphaBeta_WithMoveOrdering")]
+[TestFixture(false, false, TestName = "AlphaBeta")]
+[TestFixture(true, false, TestName = "AlphaBeta_MoveOrdering")]
+[TestFixture(false, true, TestName = "AlphaBeta_TransPosition")]
+[TestFixture(true, true, TestName = "AlphaBeta_MoveOrdering_TransPosition")]
 public class AlphaBetaMiniMaxTests : MiniMaxTestBase
 {
     private readonly bool _applyMoveOrdering;
+    private readonly bool _applyTranspositionTable;
 
-    public AlphaBetaMiniMaxTests(bool applyMoveOrdering)
+    public AlphaBetaMiniMaxTests(bool applyMoveOrdering, bool applyTranspositionTale)
     {
         _applyMoveOrdering = applyMoveOrdering;
+        _applyTranspositionTable =applyTranspositionTale;
     }
 
-    protected override IMiniMax CreateMiniMax() =>
-        new AlphaBetaMiniMax { ApplyMoveOrdering = _applyMoveOrdering };
+    protected override IMiniMax CreateMiniMax()
+    {
+        AlphaBetaMiniMax minimax = new AlphaBetaMiniMax();
+        minimax.ApplyMoveOrdering = _applyMoveOrdering;
+        minimax.ApplyTranspositionTable = _applyTranspositionTable;
+
+        return minimax;
+    }
 }
